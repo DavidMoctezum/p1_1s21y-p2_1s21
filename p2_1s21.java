952c9aab9c90eb1bd3e921e3b5c85742a1bb2d98
David Moctezuma Pérez <144501989+DavidMoctezum@users.noreply.github.com>
2023-09-08 20:37:06 -0600

    p1_1s21 y p2_1s21

---
+++ p2_1s21.java
@@ -0,0 +1,11 @@
+class p2_1s21{
+	public static void main(String[] args) {
+		System.out.println("Datos personales");
+		System.out.println("David Moctezuma Perez");
+		System.out.println("18");
+		System.out.println("davidmoc@gmail.com");
+		System.out.println("Jugar");
+		System.out.println("Morado");
+
+	}
+}
